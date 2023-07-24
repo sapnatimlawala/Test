5104dbe672e7c6ce0b7bd9c6ed0993367526ca0b
sapnatimlawala <139847602+sapnatimlawala@users.noreply.github.com>
2023-07-23 22:44:44 -0700

    Add files via upload

---
+++ led_blinky.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2019 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define BOARD_LED_GPIO     BOARD_LED_RED_GPIO
+#define BOARD_LED_GPIO_PIN BOARD_LED_RED_PIN
+
+
+
+#define BOARD_LED1_GPIO     BOARD_LED_GREEN_GPIO
+#define BOARD_LED1_GPIO_PIN BOARD_LED_GREEN_PIN
+
+#define BOARD_LED2_GPIO     BOARD_LED_ORANGE_GPIO
+#define BOARD_LED2_GPIO_PIN BOARD_LED_ORANGE_PIN
+
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+volatile uint32_t g_systickCounter;
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+void SysTick_Handler(void)
+{
+    if (g_systickCounter != 0U)
+    {
+        g_systickCounter--;
+    }
+}
+
+void SysTick_DelayTicks(uint32_t n)
+{
+    g_systickCounter = n;
+    while (g_systickCounter != 0U)
+    {
+    }
+}
+
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+    /* Board pin init */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+
+    /* Set systick reload value to generate 1ms interrupt */
+    if (SysTick_Config(SystemCoreClock / 1000U))
+    {
+        while (1)
+        {
+        }
+    }
+
+    while (1)
+    {
+        /* Delay 1000 ms */
+
+
+        SysTick_DelayTicks(1000U);
+        GPIO_PortToggle(BOARD_LED_GPIO, 1u << BOARD_LED_GPIO_PIN);
+
+       SysTick_DelayTicks(1000U);
+       GPIO_PortToggle(BOARD_LED1_GPIO, 1u << BOARD_LED1_GPIO_PIN);
+
+       SysTick_DelayTicks(1000U);
+       GPIO_PortToggle(BOARD_LED2_GPIO, 1u << BOARD_LED2_GPIO_PIN);
+
+    }
+}
